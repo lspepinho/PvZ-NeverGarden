da99d9e127608591184c32781f1272b9609a30e1
Zhou Qiankang <wszqkzqk@qq.com>
2026-01-23 11:27:23 +0800

    Feat: Enhance projectile collision logic and splash damage calculations for Emperor 1437's STAR type
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/Lawn/Projectile.cpp
+++ src/Lawn/Projectile.cpp
@@ -391,7 +391,8 @@ bool Projectile::IsSplashDamage(Zombie* theZombie)
 	return
 		mProjectileType == ProjectileType::PROJECTILE_MELON ||
 		mProjectileType == ProjectileType::PROJECTILE_WINTERMELON ||
-		mProjectileType == ProjectileType::PROJECTILE_FIREBALL;
+		mProjectileType == ProjectileType::PROJECTILE_FIREBALL ||
+		mProjectileType == ProjectileType::PROJECTILE_STAR;
 }
 
 unsigned int Projectile::GetDamageFlags(Zombie* theZombie)
@@ -406,7 +407,7 @@ unsigned int Projectile::GetDamageFlags(Zombie* theZombie)
 	{
 		SetBit(aDamageFlags, static_cast<int>(DamageFlags::DAMAGE_BYPASSES_SHIELD), true);
 	}
-	else if (mMotionType == ProjectileMotion::MOTION_STAR && mVelX < 0.0f)
+	else if (mMotionType == ProjectileMotion::MOTION_STAR && mVelX <= 0.0f)
 	{
 		SetBit(aDamageFlags, static_cast<int>(DamageFlags::DAMAGE_BYPASSES_SHIELD), true);
 	}
@@ -422,7 +423,7 @@ unsigned int Projectile::GetDamageFlags(Zombie* theZombie)
 bool Projectile::IsZombieHitBySplash(Zombie* theZombie)
 {
 	Rect aProjectileRect = GetProjectileRect();
-	if (mProjectileType == ProjectileType::PROJECTILE_FIREBALL)
+	if (mProjectileType == ProjectileType::PROJECTILE_FIREBALL || mProjectileType == ProjectileType::PROJECTILE_STAR)
 	{
 		aProjectileRect.mWidth = 100;
 	}
@@ -438,7 +439,7 @@ bool Projectile::IsZombieHitBySplash(Zombie* theZombie)
 	{
 		aRowDeviation = 0;
 	}
-	if (mProjectileType == ProjectileType::PROJECTILE_FIREBALL)
+	if (mProjectileType == ProjectileType::PROJECTILE_FIREBALL || mProjectileType == ProjectileType::PROJECTILE_STAR)
 	{
 		if (aRowDeviation != 0)
 		{
@@ -473,6 +474,10 @@ void Projectile::DoSplashDamage(Zombie* theZombie)
 	if (mProjectileType == ProjectileType::PROJECTILE_FIREBALL)
 	{
 		aMaxSplashDamageAmount = aOriginalDamage;
+	}
+	else if (mProjectileType == ProjectileType::PROJECTILE_STAR)
+	{
+		aMaxSplashDamageAmount = 2 * aOriginalDamage;
 	}
 	int aSplashDamageAmount = aSplashDamage * aZombiesGetSplashed;
 	if (aSplashDamageAmount > aMaxSplashDamageAmount)
